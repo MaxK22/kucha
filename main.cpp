13156b505d4c75c286fe6ca140c240482f095995
Maxk <korotkovmaxim.a@gmail.com>
2021-11-19 22:24:03 +0300

    E

--- main.cpp
+++ main.cpp
@@ -13,9 +13,11 @@ public:
     Kucha();
     int sift_up(int i);
     int sift_down(int i);
-    void delete_(int i);
+    int delete_(int i);
     int plus_x(int i, int x);
     int minus_x(int i, int x);
+    void extract_max();
+    int push(int x);
 };
 
 template<typename T>
@@ -48,10 +50,10 @@ int Kucha<T>::sift_down(int i) {
     int next1 = i*2 +1, next2 = i*2 +2, now = i;
     while(next1 < size)
     {
-        std::cout << "ok";//infinitive problem!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //std::cout << "ok";//infinitive problem!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         if(next2 < size)
         {
-            if(src[next1] > src[next2] && src[next1] > src[now])
+            if(src[next1] >= src[next2] && src[next1] > src[now])
             {
                 std::swap(src[next1] , src[now]);
                 now = next1;
@@ -61,6 +63,8 @@ int Kucha<T>::sift_down(int i) {
                 std::swap(src[next2] , src[now]);
                 now = next2;
             }
+            else
+                return now;
         }
         else
         {
@@ -69,6 +73,8 @@ int Kucha<T>::sift_down(int i) {
                 std::swap(src[next1] , src[now]);
                 now = next1;
             }
+            else
+                return now;
         }
         next1 = 2*now+1;
         next2 = 2*now+2;
@@ -77,11 +83,15 @@ int Kucha<T>::sift_down(int i) {
 }
 
 template<typename T>
-void Kucha<T>::delete_(int i) {
-    swap(src[i], src[size - 1]);
+int Kucha<T>::delete_(int i) {
+    int ans = src[i];
+    std::swap(src[i], src[size - 1]);
     src.pop_back();
     size--;
-    sift_down(i);
+    if(size > 1 && i < size) {
+        sift_up(sift_down(i));
+    }
+    return ans;
 }
 
 template<typename T>
@@ -96,21 +106,64 @@ int Kucha<T>::minus_x(int i, int x){
     return sift_down(i);
 }
 
+template<typename T>
+void Kucha<T>::extract_max() {
+    std::swap(src[0], src[size-1]);
+    size--;
+    src.pop_back();
+}
+
+template<typename T>
+int Kucha<T>::push(int x) {
+    src.push_back(x);
+    size++;
+    return sift_up(size-1);
+}
+
 int main() {
     Kucha<int> kucha;
-    int n,x, id;
-    std::cin >> n;
-    for(int i = 0; i < n; i++)
+    int n, m, request, data;
+    std::cin >> n >> m;
+    for(int i = 0; i < m; i++)
     {
-        std::cin >> x;
-        kucha.src.push_back(x);
-    }
-    kucha.size = kucha.src.size();
-    std::cin >> n;
-    for(int i = 0; i < n; i++)
-    {
-        std::cin >> id >> x;
-        std::cout << kucha.minus_x(id-1, x)+1 << " ";
+        std::cin >> request;
+        if(request == 1)
+        {
+            if(kucha.size == 0)
+                std::cout << -1;
+            else
+            {
+                int e = kucha.src[0];
+                if(kucha.size != 1) {
+                    kucha.extract_max();
+                    std::cout << kucha.sift_down(0) + 1 << " ";
+                }
+                else
+                {
+                    kucha.src.pop_back();
+                    kucha.size = 0;
+                    std::cout << 0 << " ";
+                }
+                std::cout << e;
+            }
+        }
+        else if(request == 2)
+        {
+            std::cin >> data;
+            if(kucha.size == n)
+                std::cout << -1;
+            else
+                std::cout << kucha.push(data)+1;
+        }
+        else  if(request == 3)
+        {
+            std::cin >> data;
+            if(kucha.size < data)
+                std::cout << -1;
+            else
+                std::cout << kucha.delete_(data-1);
+        }
+        std::cout << std::endl;
     }
     for(int i = 0; i < kucha.size; i++)
         std::cout << kucha.src[i] << " ";
